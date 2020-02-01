a530e4a553f2a5d162abed057e8462a3d001a311
itsthenap <itsthenap@gmail.com>
2020-02-01 00:42:46 -0500

    fadetoblack
    fade to black on ship repair script

--- Blue Owl Steak/Assets/Scripts/ImgFade.cs
+++ Blue Owl Steak/Assets/Scripts/ImgFade.cs
@@ -8,14 +8,43 @@ public class ImgFade : MonoBehaviour
     [SerializeField] Image load;
     [SerializeField] float fadeSpd = 1f;
     public bool IsDown;
+    bool fadingToBlack = false;
+    bool fading = false;
+    float TimePassed = 0f;
+    float TimeLimit = 2f;
+
+    public void FadeToBlack()
+    {
+        fadingToBlack = true;
+        fading = true;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (fading)
         {
-            load.color -= new Color(0, 0, 0, Time.deltaTime * fadeSpd) ;
+            load.color += new Color(0, 0, 0, (fadingToBlack ? 1 : -1) * Time.deltaTime * fadeSpd);
+            if (load.color.a >= 1)
+            {
+                fadingToBlack = false;
+                fading = false;
+                Invoke("FadeFromBlack", TimeLimit);
+            }
+            if (load.color.a <= 0)
+            {
+                fading = false;
+            }
         }
 
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    FadeToBlack();
+        //}
         IsDown = true;
+    }
+
+    public void FadeFromBlack()
+    {
+        fading = true;
     }
 }
