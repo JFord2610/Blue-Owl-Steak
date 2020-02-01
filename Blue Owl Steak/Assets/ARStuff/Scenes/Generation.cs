034f67026db378d33016cd5f0fa25ada4aef95ad
aidanthephoenix <47045951+aidanthephoenix@users.noreply.github.com>
2020-02-01 10:25:07 -0800

    Alot of stuff and tweaks

---
+++ Blue Owl Steak/Assets/ARStuff/Scenes/Generation.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Generation : MonoBehaviour
+{
+    public NavMeshSurface surface;
+    [Space(10)]
+    public List<Transform> points = new List<Transform>();
+    public List<Transform> startingpoints = new List<Transform>();
+    public Transform startingnode;
+    public List<Transform> Puzzlenodes = new List<Transform>();
+    public List<Transform> Fillernodes = new List<Transform>();
+
+    int num;
+    Transform currentlyMoving;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateWorld();
+        //surface.BuildNavMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateWorld()
+    {
+        num = Random.Range(0, startingpoints.Count);
+        startingnode.position = startingpoints[num].position;
+        points.Remove(startingpoints[num]);
+
+        for (int x = 0; x < Puzzlenodes.Count; x++)
+        {
+            num = Random.Range(0, points.Count);
+            int rotmulti = Random.Range(0, 4);
+            currentlyMoving = Puzzlenodes[x];
+            Puzzlenodes[x].position = points[num].position;
+            //Fillernodes[x].localRotation = Quaternion.Euler(0, 90 * rotmulti, 0);
+
+            for (int y = 0; y < Puzzlenodes.Count; y++)
+            {
+                DistanceChek(Puzzlenodes[x].position, Puzzlenodes[y].position);
+            }
+            points.RemoveAt(num);
+        }
+        for (int i = 0; i < Fillernodes.Count; i++)
+        {
+            num = Random.Range(0, points.Count);
+            int rotmulti = Random.Range(0, 4);
+            Fillernodes[i].position = points[num].position;
+            //Fillernodes[i].localRotation = Quaternion.Euler(0, 90 * rotmulti, 0);
+            points.RemoveAt(num);
+        }
+
+    }
+
+    void DistanceChek(Vector3 a, Vector3 b)
+    {
+        if (Vector3.Distance(a, b) <= 150 && a != b)
+        {
+            num = Random.Range(0, points.Count);
+            print("no");
+            currentlyMoving.position = points[num].position;
+            DistanceChek(currentlyMoving.position, b);
+        }
+    }
+}
---
+++ Blue Owl Steak/Assets/ARStuff/Scenes/SpawnManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    float YSpawnLoc;
+    float XSpawnLoc;
+
+    public GameObject[] EnemyTypes;
+    public float time;
+    public float tillspawn;
+    float rollforEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        SpawnFreshEnemy(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(tillspawn <= time)
+        {
+            tillspawn = Random.Range(3, 10);
+            //10 - 4, 20 - 3, 30 - 2, 40 - 1
+            rollforEnemy = Random.Range(0, 100);
+            if (rollforEnemy <= 40)
+            {
+                SpawnFreshEnemy(2);
+            }
+            else if (rollforEnemy <= 70 && rollforEnemy < 41)
+            {
+                SpawnFreshEnemy(3);
+            }
+            else if (rollforEnemy <= 90 && rollforEnemy < 71)
+            {
+                SpawnFreshEnemy(4);
+            }
+            else if (rollforEnemy <= 100 && rollforEnemy < 91)
+            {
+                SpawnFreshEnemy(5);
+            }
+            time = 0;
+        }
+    }
+
+    void SpawnFreshEnemy(int amountspawning)
+    {
+        if (EnemyTypes.Length != 0)
+        {
+            for (int i = 0; i < amountspawning; i++)
+            {
+                YSpawnLoc = Random.Range(-170, 170);
+                XSpawnLoc = Random.Range(-170, 170);
+
+
+                Instantiate(EnemyTypes[Random.Range(0, EnemyTypes.Length)], new Vector3(XSpawnLoc, 0, YSpawnLoc), Quaternion.identity);           
+            }
+        }
+        else
+        {
+            print(amountspawning);
+        }
+    }
+}
