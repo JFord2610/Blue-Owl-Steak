4418d896f2b61ea1cf5165843fe1d797b12ae4d9
ZDMGames <39507120+ZDMGames@users.noreply.github.com>
2020-02-01 20:46:02 -0500

    1 In Progress

---
+++ Blue Owl Steak/Assets/Scripts/AcidDamage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidDamage : MonoBehaviour
+{
+    public bool isActive = false;
+    public bool hasStarted = false;
+
+    public PlayerController target;
+
+    public float damage, delay;
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive == true && hasStarted == false)
+        {
+            hasStarted = true;
+            StartCoroutine(DPS());
+        }
+    }
+
+    IEnumerator DPS()
+    {
+        target.TakeDamage(damage);
+
+        yield return new WaitForSeconds(delay);
+
+        if (isActive == true){
+            StartCoroutine(DPS());
+        }
+        else
+        {
+            hasStarted = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+        {
+            if (col.GetComponent<PlayerController>() != null)
+            {
+                target = col.GetComponent<PlayerController>();
+                isActive = true;
+            }
+        }
+
+        void OnTriggerExit(Collider col)
+        {
+            if (col.GetComponent<PlayerController>() != null)
+            {
+                isActive = false;
+            }
+        }
+    }
